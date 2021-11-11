4ada73e1f47a70b84fe70f13ea51646e185ef0ff
fauziridwan1709 <evan1001t@gmail.com>
2021-11-11 21:52:31 +0700

    Update flutter test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,48 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ulaskelas/app.dart';
+import 'package:states_rebuilder/states_rebuilder.dart';
+import 'package:ulaskelas/core/_core.dart';
+import 'package:ulaskelas/main_page.dart';
+
+typedef PumperWidget = Widget Function(Widget widget);
+
+Widget material(Widget child) {
+  return Injector(
+      inject: GlobalState.injectDataMocks(),
+      builder: (context) {
+        return MaterialApp(
+          navigatorKey: nav.navigatorKey,
+          home: child,
+        );
+      });
+}
+
+Widget scaffold({
+  PreferredSizeWidget? appBar,
+  Widget? child,
+  Widget? botNav,
+}) {
+  return material(Scaffold(
+    appBar: appBar,
+    body: child,
+    bottomNavigationBar: botNav,
+  ));
+}
 
 void main() {
+  late PumperWidget widget;
+
+  setUpAll(() async {
+    widget = (Widget? child) => scaffold(
+          child: Injector(
+              inject: GlobalState.injectDataMocks(),
+              builder: (context) {
+                return child!;
+              }),
+        );
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const App());
+    await tester.pumpWidget(widget.call(const MainPage()));
   });
 }
