5e0d5763791f59c2fd20e02200148ff053817a71
bimabr <bima.br123@gmail.com>
2021-11-21 20:04:01 +0700

    Update matkul detail: - Add year Dropdown (DONE) - Add semester Dropdown (DONE) - Add review description field (DONE) - Add toast when bookmarking (DONE) - Add review status (TODO) - Add set-review-as-anon Slider(TODO)

--- lib/features/matkul/detail/presentation/pages/detail_matkul_page.dart
+++ lib/features/matkul/detail/presentation/pages/detail_matkul_page.dart
@@ -20,12 +20,9 @@ class _DetailMatkulPageState extends BaseStateful<DetailMatkulPage> {
       'elit, sed do eiusmod tempor incididunt ut labore et dolore magna '
       'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco '
       'laboris nisi ut aliquip ex ea commodo consequat.';
-  FToast ftoast = FToast();
 
   @override
-  void init() {
-    ftoast.init(context);
-  }
+  void init() {}
 
   @override
   ScaffoldAttribute buildAttribute() {
@@ -50,243 +47,232 @@ class _DetailMatkulPageState extends BaseStateful<DetailMatkulPage> {
             padding: const EdgeInsets.all(24),
             children: [
               // Title + BookmarkIcon
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Flexible(
-                    child: Text(
-                      widget.matkul,
-                      style: FontTheme.poppins20w700black(),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  OnReactive(() {
-                    return GestureDetector(
-                      onTap: () {
-                        //TODO(bim): bookmark matkul
-                        bookmark.setState((s) => s.tapMark(widget.matkul));
-                        if (bookmark.state.isMarked(widget.matkul)) {
-                          _showToast();
-                        }
-                      },
-                      child: Icon(
-                        Icons.bookmark,
-                        color: bookmark.state.isMarked(widget.matkul)
-                            ? BaseColors.goldenrod
-                            : BaseColors.gray3,
-                      ),
-                    );
-                  }),
-                ],
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Text(
-                'CSIE604273  •  4 SKS  •  Wajib SI',
-                style: FontTheme.poppins16w500black(),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              Row(
-                children: const [
-                  Tag(
-                    label: 'Tags here',
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Tag(
-                    label: 'Tags here',
-                  ),
-                  SizedBox(
-                    width: 8,
-                  ),
-                  Tag(
-                    label: 'Tags here',
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              Text(
-                lorem,
-                textAlign: TextAlign.justify,
-                style: FontTheme.poppins12w400black(),
-              ),
-              const SizedBox(
-                height: 32,
-              ),
+              _buildTitleAndBookmark(context),
+              const HeightSpace(24),
+
+              _buildMatkulTag(),
+              const HeightSpace(16),
+
+              _buildMatkulDescription(),
+              const HeightSpace(32),
               // ~~~~~~~~~~~~~~~~~~~~~~~~~
-              Text(
-                'Mata Kuliah Prasyarat',
-                style: FontTheme.poppins14w700black(),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              const Divider(
-                height: 1,
-                thickness: 1,
-                color: Color(0xFFE0E0E0),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '\u2022  Statistika dan Probabilitas',
-                      style: FontTheme.poppins12w400black(),
-                    ),
-                    const SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      '\u2022  Basis Data',
-                      style: FontTheme.poppins12w400black(),
-                    )
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 32,
-              ),
+              _buildMatkulPrerequisite(),
+              const HeightSpace(32),
 
               // ~~~~~~~~~~~~~~~~~~~~~~~~~
-              if (!ownedReview!.isEmpty())
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Ulasan Kamu',
-                      style: FontTheme.poppins14w700black(),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    const Divider(
-                      height: 1,
-                      thickness: 1,
-                      color: Color(0xFFE0E0E0),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    OnReactive(() {
-                      return ReviewCard(
-                          name: ownedReview.author!,
-                          major: 'Sistem Informasi',
-                          year: '2019',
-                          classTakenOn: ownedReview.classTakenOn!,
-                          description: 'Lorem ipsum dolor sit amet, adisplis '
-                              'consectetur adipiscing elit, sed do '
-                              'eiusmod tempor incididun ut labore et '
-                              'dolore magna aliqua. Ut enim ad minim veniam, '
-                              'quis nostrud exercitation ullamco laboris '
-                              'nisi ut aliquip ex ea commodot.',
-                          likesCount: ownedReview.likesCount!,
-                          reviewedOn: DateTime(2021, 9, 10),
-                          likesCountColor:
-                              review.state.isLiked(widget.matkul, ownedReview)
-                                  ? BaseColors.purpleHearth
-                                  : BaseColors.gray1,
-                          thumbIconColor:
-                              review.state.isLiked(widget.matkul, ownedReview)
-                                  ? BaseColors.purpleHearth
-                                  : BaseColors.gray3,
-                          onLiked: () {
-                            review.setState(
-                                (s) => s.click(widget.matkul, ownedReview));
-                          });
-                    }),
-                    const SizedBox(
-                      height: 32,
-                    ),
-                  ],
-                ),
+              if (!ownedReview!.isEmpty()) _buildReviewBySelf(ownedReview),
 
-              Text(
-                'Semua Ulasan',
-                style: FontTheme.poppins14w700black(),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              const Divider(
-                height: 1,
-                thickness: 1,
-                color: Color(0xFFE0E0E0),
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-
-              OnReactive(() {
-                return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: review.state.reviews[widget.matkul]!.map((rev) {
-                      if (rev.author != review.state.thisAuthor) {
-                        return ReviewCard(
-                          name: rev.author!,
-                          major: 'Ilmu Komputer',
-                          year: '2018',
-                          classTakenOn: rev.classTakenOn!,
-                          description: 'Lorem ipsum dolor sit amet, adisplis '
-                              'consectetur adipiscing elit, sed do '
-                              'eiusmod tempor incididun ut labore et '
-                              'dolore magna aliqua. Ut enim ad minim veniam, '
-                              'quis nostrud exercitation ullamco laboris '
-                              'nisi ut aliquip ex ea commodot.',
-                          likesCount: rev.likesCount!,
-                          reviewedOn: DateTime(2021, 9, 10),
-                          likesCountColor:
-                              review.state.isLiked(widget.matkul, rev)
-                                  ? BaseColors.purpleHearth
-                                  : BaseColors.gray1,
-                          thumbIconColor:
-                              review.state.isLiked(widget.matkul, rev)
-                                  ? BaseColors.purpleHearth
-                                  : BaseColors.gray3,
-                          imgUrl: 'test',
-                          onLiked: () {
-                            review.setState((s) => s.click(widget.matkul, rev));
-                          },
-                        );
-                      }
-                      return const SizedBox();
-                    }).toList());
-              }),
+              _buildReviews(),
             ],
           ),
         ),
-        Container(
-          padding:
-              const EdgeInsets.only(top: 12, left: 24, right: 24, bottom: 24),
-          decoration: const BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromRGBO(0, 0, 0, 0.05),
-                blurRadius: 4,
-                offset: Offset(0, -2),
+        TulisUlasanButton(
+          onTap: () => nav.goToReviewMatkulFormPage(),
+        )
+      ],
+    );
+  }
+
+  Widget _buildMatkulDescription() {
+    return Text(
+      lorem,
+      textAlign: TextAlign.justify,
+      style: FontTheme.poppins12w400black(),
+    );
+  }
+
+  Widget _buildMatkulTag() {
+    return Row(
+      children: const [
+        Tag(
+          label: 'Tags here',
+        ),
+        WidthSpace(8),
+        Tag(
+          label: 'Tags here',
+        ),
+        WidthSpace(8),
+        Tag(
+          label: 'Tags here',
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTitleAndBookmark(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: Text(
+                widget.matkul,
+                style: FontTheme.poppins20w700black(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: OnReactive(() {
+                return GestureDetector(
+                  onTap: () {
+                    //TODO(bim): bookmark matkul
+                    bookmark.setState((s) => s.tapMark(widget.matkul));
+                    if (bookmark.state.isMarked(widget.matkul)) {
+                      SuccessMessenger('Mata kuliah berhasil disimpan')
+                          .show(context);
+                    }
+                  },
+                  child: Icon(
+                    Icons.bookmark,
+                    color: bookmark.state.isMarked(widget.matkul)
+                        ? BaseColors.goldenrod
+                        : BaseColors.gray3,
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
+        const HeightSpace(8),
+        Text(
+          'CSIE604273  •  4 SKS  •  Wajib SI',
+          style: FontTheme.poppins16w500black(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildReviews() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Semua Ulasan',
+          style: FontTheme.poppins14w700black(),
+        ),
+        const HeightSpace(8),
+        const Divider(
+          height: 1,
+          thickness: 1,
+          color: Color(0xFFE0E0E0),
+        ),
+        const HeightSpace(12),
+        OnReactive(() {
+          return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: review.state.reviews[widget.matkul]!.map((rev) {
+                if (rev.author != review.state.thisAuthor) {
+                  return ReviewCard(
+                    name: rev.author!,
+                    major: 'Ilmu Komputer',
+                    year: '2018',
+                    classTakenOn: rev.classTakenOn!,
+                    description: 'Lorem ipsum dolor sit amet, adisplis '
+                        'consectetur adipiscing elit, sed do '
+                        'eiusmod tempor incididun ut labore et '
+                        'dolore magna aliqua. Ut enim ad minim veniam, '
+                        'quis nostrud exercitation ullamco laboris '
+                        'nisi ut aliquip ex ea commodot.',
+                    likesCount: rev.likesCount!,
+                    reviewedOn: DateTime(2021, 9, 10),
+                    likesCountColor: review.state.isLiked(widget.matkul, rev)
+                        ? BaseColors.purpleHearth
+                        : BaseColors.gray1,
+                    thumbIconColor: review.state.isLiked(widget.matkul, rev)
+                        ? BaseColors.purpleHearth
+                        : BaseColors.gray3,
+                    imgUrl: 'test',
+                    onLiked: () {
+                      review.setState((s) => s.click(widget.matkul, rev));
+                    },
+                  );
+                }
+                return const SizedBox();
+              }).toList());
+        }),
+      ],
+    );
+  }
+
+  Widget _buildReviewBySelf(ReviewModel ownedReview) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Ulasan Kamu',
+          style: FontTheme.poppins14w700black(),
+        ),
+        const HeightSpace(8),
+        const Divider(
+          height: 1,
+          thickness: 1,
+          color: Color(0xFFE0E0E0),
+        ),
+        const HeightSpace(12),
+        OnReactive(() {
+          return ReviewCard(
+              name: ownedReview.author!,
+              major: 'Sistem Informasi',
+              year: '2019',
+              classTakenOn: ownedReview.classTakenOn!,
+              description: 'Lorem ipsum dolor sit amet, adisplis '
+                  'consectetur adipiscing elit, sed do '
+                  'eiusmod tempor incididun ut labore et '
+                  'dolore magna aliqua. Ut enim ad minim veniam, '
+                  'quis nostrud exercitation ullamco laboris '
+                  'nisi ut aliquip ex ea commodot.',
+              likesCount: ownedReview.likesCount!,
+              reviewedOn: DateTime(2021, 9, 10),
+              likesCountColor: review.state.isLiked(widget.matkul, ownedReview)
+                  ? BaseColors.purpleHearth
+                  : BaseColors.gray1,
+              thumbIconColor: review.state.isLiked(widget.matkul, ownedReview)
+                  ? BaseColors.purpleHearth
+                  : BaseColors.gray3,
+              onLiked: () {
+                review.setState((s) => s.click(widget.matkul, ownedReview));
+              });
+        }),
+        const HeightSpace(32),
+      ],
+    );
+  }
+
+  Widget _buildMatkulPrerequisite() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Mata Kuliah Prasyarat',
+          style: FontTheme.poppins14w700black(),
+        ),
+        const HeightSpace(8),
+        const Divider(
+          height: 1,
+          thickness: 1,
+          color: Color(0xFFE0E0E0),
+        ),
+        const HeightSpace(12),
+        Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '\u2022  Statistika dan Probabilitas',
+                style: FontTheme.poppins12w400black(),
+              ),
+              const HeightSpace(4),
+              Text(
+                '\u2022  Basis Data',
+                style: FontTheme.poppins12w400black(),
               )
             ],
           ),
-          child: AutoLayoutButton(
-            text: 'Tulis Ulasan',
-            onTap: () {
-              nav.goToReviewMatkulPage();
-            },
-          ),
-        )
+        ),
       ],
     );
   }
@@ -302,36 +288,5 @@ class _DetailMatkulPageState extends BaseStateful<DetailMatkulPage> {
   @override
   Future<bool> onBackPressed() async {
     return true;
-  }
-
-  void _showToast() {
-    final Widget toast = Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: const Color(0xFFD4EFDF),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Icon(
-            Icons.check_circle,
-            color: BaseColors.success,
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-          Text('Mata kuliah berhasil tersimpan',
-              style: FontTheme.poppins12w400black()),
-        ],
-      ),
-    );
-
-    ftoast.showToast(
-      child: toast,
-      gravity: ToastGravity.TOP,
-      toastDuration: const Duration(seconds: 1),
-    );
   }
 }
--- lib/features/matkul/form/presentation/states/review_matkul_form_state.dart
+++ lib/features/matkul/form/presentation/states/review_matkul_form_state.dart
@@ -5,16 +5,30 @@ part of '_states.dart';
 class ReviewMatkulFormState {
   final formKey = GlobalKey<FormState>();
   var _formData = ReviewMatkulData();
-
+  final semesters = <String>['Semester ganjil', 'Semester genap'];
+  final years = <String>['2021', '2020', '2019', '2018', '2017'];
+  final _descController = TextEditingController();
   /// Submitting form data
   Future<void> submitForm() async {}
 
   ReviewMatkulData get formData => _formData;
+  TextEditingController get descController => _descController;
   set selectTag(List<TagModel> data) => _formData.tagData = data;
+
+  void setSemester(String sem) {
+    _formData.semester = sem;
+  }
+  void setYear(String year) {
+    _formData.year = year;
+  }
+  void setDesc(String desc) {
+    _formData.description = desc;
+  }
 
   /// Cleaning form when success submitting form
   void cleanForm() {
     _formData = ReviewMatkulData();
+    _descController.text = '';
     searchTag.state.cleanSearch();
   }
 }
@@ -25,4 +39,7 @@ class ReviewMatkulData {
   }
 
   late List<TagModel> tagData;
+  String? semester;
+  String? year;
+  String? description;
 }
