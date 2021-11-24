96b57fb424697dc6bc483f6fbf189d7800dd27c6
bimabr <bima.br123@gmail.com>
2021-11-25 00:54:11 +0700

    Update Review Form: - Add flutter_switch package - Add set-to-anon toggle (DONE) - Add form validation (DONE)

--- lib/features/matkul/form/presentation/states/review_matkul_form_state.dart
+++ lib/features/matkul/form/presentation/states/review_matkul_form_state.dart
@@ -8,21 +8,30 @@ class ReviewMatkulFormState {
   final semesters = <String>['Semester ganjil', 'Semester genap'];
   final years = <String>['2021', '2020', '2019', '2018', '2017'];
   final _descController = TextEditingController();
+
   /// Submitting form data
   Future<void> submitForm() async {}
 
   ReviewMatkulData get formData => _formData;
+
   TextEditingController get descController => _descController;
+
   set selectTag(List<TagModel> data) => _formData.tagData = data;
 
   void setSemester(String sem) {
     _formData.semester = sem;
   }
+
   void setYear(String year) {
     _formData.year = year;
   }
+
   void setDesc(String desc) {
     _formData.description = desc;
+  }
+
+  void setAnon(bool val) {
+    _formData.isAnonymous = val;
   }
 
   /// Cleaning form when success submitting form
@@ -36,10 +45,12 @@ class ReviewMatkulFormState {
 class ReviewMatkulData {
   ReviewMatkulData() {
     tagData = [];
+    isAnonymous = false;
   }
 
   late List<TagModel> tagData;
   String? semester;
   String? year;
   String? description;
+  bool? isAnonymous;
 }
