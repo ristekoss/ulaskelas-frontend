7c69d75940be13e5ed704fe071ac6804b846e6e4
fauziridwan1709 <evan1001t@gmail.com>
2021-11-28 09:20:34 +0700

    Add Base Url

--- lib/core/environment/endpoints.dart
+++ lib/core/environment/endpoints.dart
@@ -2,5 +2,12 @@ part of '_environment.dart';
 
 class Endpoints {
   ///Base Endpoints
-  static const user = 'users';
+  static const baseUrlDev =
+      'http://ulaskelas-dev.ap-southeast-1.elasticbeanstalk.com';
+  static const baseUrlProd =
+      'http://ulaskelas-dev.ap-southeast-1.elasticbeanstalk.com';
+  static const sso = 'login';
+  static const users = 'users';
+  static const userId = 'users/{{Id}}';
+  static const review = 'reviews';
 }
