b9bdf70869840bd31af08da499ca14790d9a868a
fauziridwan1709 <evan1001t@gmail.com>
2021-12-01 11:23:26 +0700

    Add Failures Add API Call Add Bookmark State Add Success Messenger on remove Bookmarks

---
+++ lib/core/client/_client.dart
@@ -0,0 +1,10 @@
+// Created by Muhamad Fauzi Ridwan on 30/11/21.
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+import 'package:ulaskelas/core/error/_error.dart';
+
+part 'api_call.dart';
--- lib/features/matkul/bookmarks/presentation/widgets/_widgets.dart
+++ lib/features/matkul/bookmarks/presentation/widgets/_widgets.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ristek_material_component/ristek_material_component.dart';
+import 'package:ulaskelas/core/bases/states/_states.dart';
 import 'package:ulaskelas/core/theme/_theme.dart';
 import 'package:ulaskelas/features/matkul/search/data/models/_models.dart';
 
