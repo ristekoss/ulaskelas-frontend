aeabcf38847ca5e6df4768df04f4d7fa4b754b87
adietyachristian <adietya.christian@ui.ac.id>
2021-12-09 13:12:40 +0700

    Add deleteReview Method

--- lib/features/matkul/detail/presentation/states/_states.dart
+++ lib/features/matkul/detail/presentation/states/_states.dart
@@ -1,4 +1,7 @@
 // Created by Muhamad Fauzi Ridwan on 07/11/21.
+import 'package:dio/dio.dart';
+import 'package:ulaskelas/core/client/_client.dart';
+import 'package:ulaskelas/core/environment/_environment.dart';
 import 'package:ulaskelas/features/matkul/detail/data/models/_models.dart';
 
 part 'review_state.dart';
