c6afed1132de27e950a6dfc01f3b880a296371fb
Yudha Haris Purnama <yudha.haris@ui.ac.id>
2021-12-10 10:48:49 +0700

    reimplement like

--- lib/features/matkul/detail/presentation/states/_states.dart
+++ lib/features/matkul/detail/presentation/states/_states.dart
@@ -1,10 +1,9 @@
 // Created by Muhamad Fauzi Ridwan on 07/11/21.
-import 'dart:convert';
 
-import 'package:http/http.dart' as http;
-import 'package:ulaskelas/core/client/_client.dart';
-import 'package:ulaskelas/core/environment/_environment.dart';
+import 'package:ulaskelas/features/matkul/detail/data/datasources/_datasources.dart';
 import 'package:ulaskelas/features/matkul/detail/data/models/_models.dart';
+import 'package:ulaskelas/features/matkul/detail/data/repositories/_repositories.dart';
+import 'package:ulaskelas/features/matkul/detail/domain/repositories/_repositories.dart';
 
 part 'bookmark_state.dart';
 part 'review_state.dart';
