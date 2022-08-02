17d040ba38c9ca967a706be13f9a707d8ee1769a
Veloraine <bivanderr@gmail.com>
2022-08-02 15:49:58 +0700

    fix: removed unecessary imports

--- lib/features/kalkulator/presentation/widgets/_widgets.dart
+++ lib/features/kalkulator/presentation/widgets/_widgets.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ristek_material_component/ristek_material_component.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
