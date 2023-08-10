f4605df61fecb448949cbbe916ecb1a1bbd89712
Yudha Haris Purnama <yudha@clade.ventures>
2023-08-10 11:58:22 +0700

    fix: build runner in repository

--- lib/core/environment/secrets.dart
+++ lib/core/environment/secrets.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'secrets.g.dart';
 
-@Envied(name: 'Secret', path: '.env')
+@Envied(name: 'Secret', path: '.env.example')
 abstract class Secret {
   @EnviedField(varName: 'BASE_URL_DEV')
   static const String baseUrlDev = _Secret.baseUrlDev;
