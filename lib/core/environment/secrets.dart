be009da7d85a01504089485f53cd92e6465f2aac
Veloraine <bivanderr@gmail.com>
2023-11-26 20:04:23 +0700

    fix: change secrets env target path

--- lib/core/environment/secrets.dart
+++ lib/core/environment/secrets.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'secrets.g.dart';
 
-@Envied(name: 'Secret', path: '.env.example')
+@Envied(name: 'Secret', path: '.env')
 abstract class Secret {
   @EnviedField(varName: 'BASE_URL_DEV')
   static const String baseUrlDev = _Secret.baseUrlDev;
